a2db05a93dd26bfe546419bea7de28691594cad1
modiamar <modiamar@udel.edu>
2017-04-15 20:15:16 -0400

    Now I have the surrounding mine points displaying the numbers thing Minewseeper is so weird

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -15,9 +15,12 @@ class Grid
 {
  public:
     bool has_bomb, marked;
+    int adjacent_bombs;
     Grid(void) {
         has_bomb = false;
         marked = false;
+        adjacent_bombs = 0;
+
     }
 };
 
@@ -43,7 +46,37 @@ void drawBoard(Grid board[sizex][sizey])
            }
            else if (board[x][y].marked)
            {
+            if (board[x][y].marked and board[x][y].adjacent_bombs != 0)
+           {
+                if (x == 0 or x == 9 or y == 9 or y == 0)
+                {
+                    if (board[x][y].adjacent_bombs -1 == 0)
+                    {
+                        cout << "*|";
+                    }
+                    else
+                    {
+                        cout << board[x][y].adjacent_bombs - 1;
+                        cout <<  "|";
+                    }
+                }
+                else
+                {
+                    cout << board[x][y].adjacent_bombs;
+                    cout <<  "|";
+                }
+                
+
+           }
+           else
+           {
             cout << "*|";
+           }
+            
+           }
+           else if (board[x][y].has_bomb)
+           {
+            cout << "b|";
            }
            else
            {
@@ -82,10 +115,54 @@ int main()
         cin >> checkx;
         cout << "Input a y value to check" << endl;
         cin >> checky;
-
+        int i = 0;
+        int bombcount = 0;
         if (gameboard[checkx][checky].marked == false)
         {
             gameboard[checkx][checky].marked = true;
+
+            while (gameboard[checkx + i][checky].has_bomb !=true)
+            {;
+                gameboard[checkx + i][checky].marked = true;
+                if (gameboard[checkx+ i + 1][checky].has_bomb)
+                {
+                    bombcount++;
+                }
+                if (gameboard[checkx + i][checky - 1].has_bomb)
+                {
+                    bombcount ++;
+                }
+                if (gameboard[checkx + i][checky + 1].has_bomb)
+                {
+                    bombcount ++;
+                }
+                gameboard[checkx + i][checky].adjacent_bombs = bombcount;
+                bombcount = 0;
+                i++;
+                }
+            bombcount = 0;
+            int i = 0;
+            while (gameboard[checkx - i][checky].has_bomb !=true)
+            {;
+                gameboard[checkx - i][checky].marked = true;
+                if (gameboard[checkx- i - 1][checky].has_bomb)
+                {
+
+                    bombcount++;
+                }
+                if (gameboard[checkx - i][checky - 1].has_bomb)
+                {
+                    bombcount ++;
+                }
+                if (gameboard[checkx -i][checky + 1].has_bomb)
+                {
+                    bombcount ++;
+                }
+                gameboard[checkx - i][checky].adjacent_bombs = bombcount;
+                bombcount = 0;
+                i++;
+                }
+
             if (gameboard[checkx][checky].has_bomb == true)
             {
                 lose = true; 
